6c7efd656852b068df9104c6fab7b1e26f4f2a08
yusufacik26 <yusufacikedu@gmail.com>
2023-11-16 22:39:05 +0300

    My daily work
    Today ı just studied my midterm exam.I tried a "BRO NUM" alghoritms .

---
+++ JavaApplication83/src/javaapplication83/JavaApplication83.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication83;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication83 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  int[] dizi =new int[]{1,2,3,4,5};
+        int aranan=5;
+   for(int i=1;i<=dizi[i];i++){
+   if(aranan==dizi[i]){System.out.println("Bu sayı bulunmaktadır ve indisi "+i+" dır");
+   break;
+   }
+   }
+   int num1=220;
+   int num2=284;
+   int top1=0;
+   int top2=0;
+   
+   for(int a =1;a<num1;a++){
+       if(num1%a==0)top1+=a;
+   }
+   for(int b=1;b<num2;b++){
+    if(num2%b==0)top2+=b;
+   }
+   if(top1==num2&&top2==num1)System.out.println("BRO NUMBER");
+   
+   }
+        
+       
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
