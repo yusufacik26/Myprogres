4f89dcaa873816b73a0269853b51bcd94bc93a07
yusufacik26 <yusufacikedu@gmail.com>
2023-11-18 18:44:48 +0300

    MyDailyWork
    Today ı worked my midretm exam again so that's my daily job.

---
+++ JavaApplication87/src/javaapplication87/JavaApplication87.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication87;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication87 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner k =new Scanner(System.in);
+   /*     System.out.println("iki sayı giriniz");
+     int a=k.nextInt();
+     int b =k.nextInt();
+     int bolen=0;
+     if(a < b)
+     for(int i= 2;i <= a; i++){
+     if(a % i == 0 && b % i==0){
+     bolen= i;
+     }
+     }
+     else {
+         for(int j = 2;j <=b; j++){
+     if(a % j ==0 && b % j== 0){
+     bolen= j ;
+     }
+     
+     }
+         
+     }
+     
+      System.out.println(bolen);*/
+        
+  /*      System.out.println("Please enter a sentence...");
+        String stc=k.nextLine();
+        int wordcount=1;
+        for(int i=0;i<stc.length()-1;i++){
+        char c =stc.charAt(i);
+        if(c==' ') wordcount++;
+        
+        
+        }
+        System.out.println("This sentence has "+wordcount+" word."); */
+      /*  System.out.println("Please enter e word");
+        String str=k.nextLine();
+        System.out.println("Please choose a character");
+        char c=k.next().charAt(0);
+  int count=0;
+  for(int i =0;i<str.length();i++){
+  if(str.charAt(i)==c)count++;
+  
+  
+  
+  
+  
+  
+  }
+        System.out.println("This word have "+count+" "+c+" character");*/
+    /*    System.out.println("Bir kelime gir");
+        String c1=k.nextLine();
+        String c2="";
+      for(int i=c1.length()-1;i>=0;i--) {
+      char c =c1.charAt(i);
+          c2+=c;
+      
+      
+      }
+        if(c1.equals(c2))System.out.println("polindrom kelime");
+        else System.out.println("değil");*/
+  /*  Random random=new Random();
+    int sayi;
+    int sayac;
+    for(int i=2;i<=20;i++){
+   
+        sayi=1+random.nextInt(100);
+         sayac=0;
+    for(int j =2;j<sayi;j++){
+    if(sayi % j ==0)sayac++;
+   
+    
+    }
+ if(sayac ==0)System.out.println("PRİME NUM==> "+sayi);
+ if (sayi % 2 == 0)System.out.println("EVEN NUM ==> "+sayi);   
+ if(sayi%2!=0) System.out.println("ODD NUM==> "+sayi );
+    
+    }
+*/
+  /*int a =1;
+  for(int i =1;i<5;i++){
+      
+   for(int j =1;j<=i;j++){
+
+       System.out.print(a);
+       System.out.print(" ");
+   a+=2;
+   }
+      System.out.println("");
+  
+  }*/
+        
+ /* int a=k.nextInt();
+  while(a>0){
+      System.out.print(a%10);
+  a=a/10;
+  }*/
+    /*    System.out.println("Please enter a num");
+  int a=k.nextInt();
+  int fak=1;
+  for(int i=1;i<=a;i++){
+      
+   fak*=i;
+  System.out.println(i+"! ="+fak);
+    
+  
+  
+  }*/
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    }
+}
+
