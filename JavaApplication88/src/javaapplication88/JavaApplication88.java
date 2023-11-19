8b4137d44b3dcbf05727d7562d6714f9faecb067
yusufacik26 <yusufacikedu@gmail.com>
2023-11-19 20:58:34 +0300

    WorkJob
    daily

---
+++ JavaApplication88/src/javaapplication88/JavaApplication88.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication88;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication88 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Scanner k=new Scanner(System.in);
+        
+  /*  System.out.println("Bir sayı giriniz");
+  int a ;
+  int top=0;
+  
+  for(int i = 0; i<10 ; i++){ 
+      top=0;
+      a=k.nextInt();
+      
+      while(a!=0){
+          top+=a;
+          System.out.println("Bir sayı giriniz");
+          a=k.nextInt();
+           
+          
+      
+      }
+  System.out.println(top);
+  }
+        */
+   /*     System.out.println("bir string girin");
+  String s=k.nextLine();
+  s=s.replace(" ","");
+  
+  for(int a =s.length()-1;a>=0;a--){
+  
+  char c =s.charAt(a);
+      System.out.print(c);
+  
+  }
+  
+  
+  }*/
+     /*   System.out.println("İki string giriniz");
+       String s1=k.nextLine();
+       String s2=k.nextLine();
+       String s3="";
+        for(int i=0;i<s1.length();i++){
+            char c1=s1.charAt(i);
+            
+            char c2=s2.charAt(i);
+            if(c1!=c2){s3+=c1+c2;}
+            
+        }
+        System.out.println(s3);
+  */  
+   /*  int a =1;
+   for(int i=0;i<7;i++){
+   for(int j =0;j<i;j++){
+       System.out.print(a);
+       System.out.print(" ");
+  a=a+2;
+   }
+       System.out.println("");
+   
+   }*/
+ 
+     
+     
+     
+    }}
+     
+   
+ 
+    
+    
