a3985f01912b3731388ea883dea5168fa99732cd
yusufacik26 <yusufacikedu@gmail.com>
2023-11-20 23:07:49 +0300

    Midterm exercise
    Just daily ı found some questions and ı solved on my way.

---
+++ JavaApplication89/src/javaapplication89/JavaApplication89.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication89;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication89 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   /*   int sonuc =1;
+      int aratoplam=0;
+     for(int x =1;x<3;x++){
+     for(int y=2;y<4;y++){
+     aratoplam+=(x+y);
+     
+     
+     }
+     sonuc*=aratoplam;
+     aratoplam=0;
+     
+
+     
+     
+     }
+        System.out.println(sonuc);*/
+   Scanner k =new Scanner(System.in); 
+   /*double pi=3.14;
+        System.out.println("Yarıçapı giriniz");
+    int yariCap=k.nextInt();
+    double daireAlan=(2*pi*yariCap);
+        System.out.println("Dairenin alanı = "+daireAlan);*/
+    /*    System.out.println("Bir yaş giriniz");
+        int yas=k.nextInt();
+        int eb=yas;
+        int buyuksayac=0;
+        int ek =yas;
+        int kucuksayac=0;
+    for(int i =1;i<=9;i++){
+    yas=k.nextInt();
+    
+    if(yas>eb) {eb=yas; buyuksayac=i+1;}
+    
+    if(yas<ek){ek=yas; kucuksayac=i+1;}
+    
+    }
+        System.out.println(eb+" sırası ise "+buyuksayac+"dır" );
+        System.out.println(ek+" sırası ise  "+kucuksayac+"dır");
+*/    
+     /*   System.out.println("Bir sayı giriniz");
+        int a =k.nextInt();
+        for(int i=0;i<=a;i++){
+         for(int j =0;j<i;j++){
+             System.out.print("&");
+          
+         
+         }
+            System.out.println("");
+        
+        
+        
+        }*/
+  /*      System.out.println("Bir string giriniz");
+     String str=k.nextLine();
+     int sayi=0;
+     int harf=0;
+     int karakter=0;
+    
+    
+for(int i=0;i<str.length();i++){
+if(str.charAt(i)>='a'&& str.charAt(i)<='z'|| str.charAt(i)>='A'&& str.charAt(i)<='Z'){
+harf++; System.out.println("Bu stringin "+(i+1)+ ". elemanı harftir");
+
+
+
+
+}
+else if (str.charAt(i)>='1'&& str.charAt(i)<='9'){
+sayi++;
+    System.out.println("Bu stringin "+(i+1)+" . elemanı sayıdır");
+
+}
+else {System.out.println("Bu stringin "+(i+1)+" . elemanı özel karakterdşr");
+}
+
+
+
+}    
+        System.out.println("Bu stringte "+harf+" kadar harf "+sayi+" kadar sayı "+karakter+" kadar özel karakter bulunmaktadır.");
+    
+    */
+  /*int sayac=0;
+        System.out.println("Bir string giriniz");
+        String s =k.nextLine();
+        for(int i =0;i<s.length();i++){
+        if(s.charAt(i)==' ')  sayac++;
+        
+        
+        
+        
+        
+        
+        }
+        System.out.println("Boşluk sayısı = "+sayac);*/
+  
+  
+ // int a =99;
+  //int top=0;
+  //int sayac=1;
+/*  while (sayac<=10){
+  top+=a;
+  
+  sayac++;
+  
+  a-=5;
+  
+  
+  
+  
+  }*/
+     //   System.out.println("toplamınız "+top);
+    /*while(sayac<=405){
+    sayac++;
+        a-=5;
+        
+   
+    if(sayac==405) System.out.println(a-5);
+    
+    
+   */
+   /*     System.out.println("Bir cümle veya kelime giriniz");
+    String s =k.nextLine();
+    for(int i =0;i<s.length();i++){
+        System.out.println(s.charAt(i));
+    
+    
+    }*/
+  /*  int tek =0;
+    int cift=0;
+    int gtop=0;
+    int a =5;
+    for(int i=1;i<=5;i++){
+        if(i%2==0)cift+=i;
+        else tek+=i;
+    gtop+=i;
+    
+    
+    
+    
+    }
+        System.out.println(tek+" "+" "+cift+" "+gtop);*/
+   
+     
+
+    
+    
+}
+}
