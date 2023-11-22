5f4e65daf5255b653cf1f3dfc681f51fbdc5c592
yusufacik26 <yusufacikedu@gmail.com>
2023-11-22 16:43:24 +0300

    fibonacci

---
+++ fibonacci/src/javaapplication108/JavaApplication108.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication108;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication108 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a =9;
+        int num1=0;
+        int num2=1;
+        int num3=0;
+        System.out.print("0"+" 1");
+        for(int i =0;i<a;i++){
+        num3=num1+num2;
+        num1=num2;
+        num2=num3;
+            System.out.print(" "+num3);
+        
+        
+        
+        
+        
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
