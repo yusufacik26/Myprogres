b36e607a7d05c8b95f1ad5ba303515072c3619e6
yusufacik26 <yusufacikedu@gmail.com>
2023-11-25 14:44:02 +0300

    random sayılarda 10 girilene kadar en büyüğü bulan

---
+++ random sayılarda 10 girilene kadar en büyüğü bulan/src/iki/sayının/bölme/işlemini/çıkartma/ike/yap/İkiSayınınBölmeIşleminiÇıkartmaIkeYap.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iki.sayının.bölme.işlemini.çıkartma.ike.yap;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class İkiSayınınBölmeIşleminiÇıkartmaIkeYap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner kb=new Scanner(System.in);
+     Random r =new Random();
+  int eb=0;   
+    int a ;
+    for(;;){
+     a=r.nextInt(10)+1;
+    if(a==10)break;
+    else if (a>eb)eb=a;
+
+    }
+        System.out.println("en büyük sayı  = "+eb);
+    
+    }
+}
