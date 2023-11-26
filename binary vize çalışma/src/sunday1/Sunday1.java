5937027524443e42a9e235ee658e378d7e9ff19c
yusufacik26 <yusufacikedu@gmail.com>
2023-11-26 15:58:48 +0300

    binarye çevirme vize çalışma

---
+++ binary vize çalışma/src/sunday1/Sunday1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sunday1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class Sunday1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner k=new Scanner(System.in);
+        System.out.println("Onluk Bir sayı girin");
+ int s=k.nextInt();
+ String bos="";
+        while(s>0){
+    int kalan=s%2;
+            bos= kalan+bos;
+        s=s/2;
+        }
+
+        System.out.println(bos);
+
+    }
+
+
+}
+
+
