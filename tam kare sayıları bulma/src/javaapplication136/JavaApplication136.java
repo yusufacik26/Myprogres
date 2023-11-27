a76478604889c7ab6c1cbcd36c87d1e9f357e6d3
yusufacik26 <yusufacikedu@gmail.com>
2023-11-27 18:31:53 +0300

    Tam sayıları bulan program
    for döngüsü ile 10 ile 100 e kadar olan sayıların tam kare olanları bulma

---
+++ tam kare sayıları bulma/src/javaapplication136/JavaApplication136.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication136;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+      Scanner ka=new Scanner(System.in);
+   for(int i =10;i<100;i++){
+    for(int j=1;j<10;j++){
+        if((j*j)==i) System.out.println(i+"  sayısı tam kare sayıdır");
+
+       }
+   
+   
+   }
+    }
+    
+}
