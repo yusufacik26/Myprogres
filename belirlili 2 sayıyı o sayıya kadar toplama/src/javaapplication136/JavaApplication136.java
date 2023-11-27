0f0812a5620942bcba837c8dac52ae9948e62330
yusufacik26 <yusufacikedu@gmail.com>
2023-11-27 15:09:17 +0300

    belirlili 2 sayıyı o sayıya kadar toplama
    2 sayı belirledik m ve k diye bu sayıları 1den başlayıp kendisine kadar topladık.

---
+++ belirlili 2 sayıyı o sayıya kadar toplama/src/javaapplication136/JavaApplication136.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication136;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YusufOpens
+ */
+public class JavaApplication136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ 
+      Scanner ka=new Scanner(System.in);
+      int top=0;  
+      for(int k=1;k<=5;k++){
+         for(int m =1;m<=3;m++){
+          top+=(k+m);
+         
+         
+         }
+      
+      
+      
+      }
+        System.out.println("sonucunuz "+top);
+        
+        
+        
+    }
+    
+}
