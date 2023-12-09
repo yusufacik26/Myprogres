64f461b5fd6eb40732d73e416b0ddc0d1e6897b3
yusufacik26 <yusufacikedu@gmail.com>
2023-12-09 18:52:07 +0300

    Dizilere girişş

---
+++ Arrays/src/arrays/Arrays.java
@@ -0,0 +1,32 @@
+
+package arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+    // double[] list1;
+     //list1=new double[10];
+       double list[]=new double[5]; 
+      /* list[0]=1;
+        list[1]=3;
+        list[2]=14;        
+        list[3]=11;
+        list[4]=123;
+        System.out.println(list[2]);*/
+      String days[]={"Yusuf","Acik","CEng"};
+      days[0]="Joseph"; // Sıfırıncı indisteki değeri bu şekilde değiştirebiliyoruz indisi kendimize göre değiştirebiliyoruz
+        System.out.println(days[0]);
+        System.out.println(days.length);
+        for(int i =0;i<=days.length-1;i++){
+            System.out.println(days[i]);
+        
+        }
+      
+      
+      
+      
+      
+      
+    }
+    
+}
