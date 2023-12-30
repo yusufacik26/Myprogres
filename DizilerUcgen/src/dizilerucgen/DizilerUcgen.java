4ca6f0ca405e5d0a0f8d09608c41c71e04dd083a
yusufacik26 <yusufacikedu@gmail.com>
2023-12-30 12:49:52 +0300

    TekBoyutluDizi

---
+++ DizilerUcgen/src/dizilerucgen/DizilerUcgen.java
@@ -0,0 +1,40 @@
+package dizilerucgen;
+
+import java.util.Scanner;
+
+public class DizilerUcgen {
+
+    public static void main(String[] args) {
+Scanner k =new Scanner(System.in) ;
+    int d[]={1,2,3,4,5};
+    int a=d.length;
+        for (int i = 0; i < d.length;i++) {
+            for(int j=0;j<a;j++){
+                System.out.print(d[j]+" ");
+            
+            
+            }
+            System.out.println();
+            a--;
+        }
+    
+    
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
