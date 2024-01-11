26301b68afb71badf7522320227605448420ab0d
yusufacik26 <yusufacikedu@gmail.com>
2024-01-11 18:05:26 +0300

    Ebob metod

---
+++ EbobMetod/src/ekokmetod/EkokMetod.java
@@ -0,0 +1,50 @@
+
+package ekokmetod;
+
+import java.util.Scanner;
+public class EkokMetod {
+    
+    public static int ekok(int a,int b ){
+    int ek=1;
+    
+    if(a==b) ek=a;
+    
+    if(a<b){
+        for(int i =1;i<=b;i++){
+          if(a%i==0 && b%i ==0) ek=i;
+        
+        }
+
+             }  
+     else if (a>b) {
+             for (int i = 0; i < a; i++) {
+              if(a%i==0 && b%i ==0) ek=i;
+}
+    }
+    
+   
+        System.out.println(ek);
+    
+    
+    return ek;
+    }
+
+    
+    public static void main(String[] args) {
+        Scanner k= new Scanner(System.in);
+        int a=k.nextInt();
+        int b = k.nextInt();
+    
+        ekok(a,b);
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
