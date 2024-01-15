cd13615742a6f08f19bb1355ccbe79533b8eff85
yusufacik26 <yusufacikedu@gmail.com>
2024-01-15 10:58:55 +0300

    SonFinalTekrar4

---
+++ Pratik4/src/pratik/Pratik.java
@@ -0,0 +1,55 @@
+
+package pratik;
+
+import java.util.Scanner;
+
+
+public class Pratik {
+    
+    
+    
+    public static int aSayısı(String s ){
+
+int ct=0;
+for(int i = 0 ; i<s.length();i++){
+
+    
+    char c = s.charAt(i);
+        if(c=='a') ct++;
+
+
+}
+        System.out.println(ct);
+return ct;
+}
+
+
+
+    public static void main(String[] args) {
+  Scanner k = new Scanner(System.in);
+ 
+  String s = k.nextLine();
+ aSayısı(s);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+       
+       
+    }
+    
+
