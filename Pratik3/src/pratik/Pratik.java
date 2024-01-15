9342c257df88c4a1aa129326967058905712e91c
yusufacik26 <yusufacikedu@gmail.com>
2024-01-15 10:50:41 +0300

    SonFinalTekrar3

---
+++ Pratik3/src/pratik/Pratik.java
@@ -0,0 +1,47 @@
+
+package pratik;
+
+import java.util.Scanner;
+
+
+
+
+public class Pratik {
+
+    public static void main(String[] args) {
+  Scanner k = new Scanner(System.in);
+ 
+  String s = k.nextLine();
+  
+  do{
+  
+   if(s.charAt(0)=='A'|| s.charAt(0)=='a') System.out.println(s);
+   s = k.nextLine();
+  
+  }
+  while(s.charAt(0)!='q');
+  
+  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+       
+       
+    }
+    
+
